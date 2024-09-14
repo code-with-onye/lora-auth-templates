6fca93bca4484fe15473fdc40c7e3d6a26505dd0
Trust Onyemachi <85189857+code-with-onye@users.noreply.github.com>
2024-09-14 17:53:24 +0100

    added axios.ts and schema.ts

--- example/src/hooks/server/useSignin.ts
+++ example/src/hooks/server/useSignin.ts
@@ -1,16 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import { z } from "zod";
 
-import { useUserStore } from "../client/useSignIn";
-import { SignUserSchema, UserSchema } from "@/schema";
-import axios from "@/utils/axios";
+import { SignInSchema } from "@/components/shared/forms/signin-form";
+import { SiginIn } from "@/endpoint/user";
+import { UserSchema } from "@/schema/user";
+
+import { useUserStore } from "../client/store/useUser";
 
 export const useSignIn = () => {
   const { login } = useUserStore();
   const { mutate, isPending, isError, error } = useMutation({
     mutationKey: ["user_signin"],
-    mutationFn: (data: z.infer<typeof SignUserSchema>) =>
-      axios.post("/user/signin", data),
+    mutationFn: (data: z.infer<typeof SignInSchema>) => SiginIn(data),
     onSuccess: (data: any) => {
       const user: z.infer<typeof UserSchema> = data?.data?.data?.user;
       login({ ...user, token: data?.data?.data?.token });
